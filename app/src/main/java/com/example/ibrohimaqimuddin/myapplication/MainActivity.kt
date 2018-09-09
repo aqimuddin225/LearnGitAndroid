ca127b9f02d26e5e6c1306796f2ab67e7cdf4fce
Ibrohim <ibrohimaqimuddin@gmail.com>
2018-09-10 01:35:33 +0700

    Add some comment, then push it to GitHub

--- app/src/main/java/com/example/ibrohimaqimuddin/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/ibrohimaqimuddin/myapplication/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.ibrohimaqimuddin.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-
+// I Just try to add comment and push it to GitHub
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
